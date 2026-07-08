87c5835c9592007178ca832b3681d81355bfcd9d
Sayandip Adhikary <sayandipadhikary@Sayandips-MacBook-Air.local>
2026-07-08 10:24:57 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main className='h-screen w-screen flex items-center justify-center'>
+      <h1 className='text-3xl font-medium animate-bounce'>Expense Tracker Web</h1>
+    </main>
+  )
+}
+
+export default App
